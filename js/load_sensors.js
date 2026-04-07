7733db850c0baedab4afd566b1fd60c45410a397
DKevinM <35807537+DKevinM@users.noreply.github.com>
2026-04-07 13:17:17 -0600

    Update load_sensors.js

--- js/load_sensors.js
+++ js/load_sensors.js
@@ -2,50 +2,64 @@ const SUPABASE_URL = "https://zcunoncbyitfsilrhymv.supabase.co"
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjdW5vbmNieWl0ZnNpbHJoeW12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3Mjk3NDYsImV4cCI6MjA2NzMwNTc0Nn0._z_tqm_5UIBkWfMa7HAJrUOA-0t9vOaBVV48-74esWQ"
 
 async function loadSensors(){
+
     const url = `${SUPABASE_URL}/rest/v1/latest_sensor_readings`
+
     const response = await fetch(url, {
         headers: {
             "apikey": SUPABASE_KEY,
             "Authorization": `Bearer ${SUPABASE_KEY}`,
         }
     })
+
     if (!response.ok) {
         const err = await response.text()
         console.error("SUPABASE ERROR:", err)
         return
     }
+
     const data = await response.json()
+
     console.log("DATA:", data)
+
     buildTable(data)
 }
 
 
+function buildTable(data){
 
-data
-  .filter(s => s.name.includes("ACA") || s.name.includes("WCAS"))
-  .forEach(s => {
+    const tbody = document.querySelector("#sensorTable tbody")
 
-    const qc = qcCheck(s.pm25_atm_a, s.pm25_atm_b)
+    tbody.innerHTML = ""
 
-    const hoursOld = (Date.now() - new Date(s.recorded_at)) / 3600000
+    data
+      .filter(s => s.name.includes("ACA") || s.name.includes("WCAS"))
+      .forEach(s => {
 
-    let status = qc.status
-    if (hoursOld > 3) status = "OFFLINE"
+        const qc = qcCheck(s.pm25_atm_a, s.pm25_atm_b)
 
-    const tr = document.createElement("tr")
+        const hoursOld = (Date.now() - new Date(s.recorded_at)) / 3600000
 
-    tr.innerHTML = `
-    <td>${s.name}</td>
-    <td>${new Date(s.recorded_at).toLocaleString("en-CA", {
-        timeZone: "America/Edmonton"
-    })}</td>
-    <td>${s.pm25_atm_a ?? "-"}</td>
-    <td>${s.pm25_atm_b ?? "-"}</td>
-    <td>${s.pm_corrected ?? qc.avg ?? "-"}</td>
-    <td>${qc.diff ?? "-"}</td>
-    <td class="${status}">${status}</td>
-    `
+        let status = qc.status
+        if (hoursOld > 3) status = "OFFLINE"
 
-    tbody.appendChild(tr)
-})
+        const tr = document.createElement("tr")
 
+        tr.innerHTML = `
+        <td>${s.name}</td>
+        <td>${new Date(s.recorded_at).toLocaleString("en-CA", {
+            timeZone: "America/Edmonton"
+        })}</td>
+        <td>${s.pm25_atm_a ?? "-"}</td>
+        <td>${s.pm25_atm_b ?? "-"}</td>
+        <td>${s.pm_corrected ?? qc.avg ?? "-"}</td>
+        <td>${qc.diff ?? "-"}</td>
+        <td class="${status}">${status}</td>
+        `
+
+        tbody.appendChild(tr)
+    })
+}
+
+loadSensors()
+setInterval(loadSensors, 60 * 60 * 1000)
